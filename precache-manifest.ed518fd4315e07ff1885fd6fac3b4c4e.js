d01c002bec083086226787bfa7363bb91ffa2cfc
olgab <olgabresheva7@gmail.com>
2020-07-26 21:41:03 -0700

    Updates

---
+++ precache-manifest.ed518fd4315e07ff1885fd6fac3b4c4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "914101563ffa13d4d7ff12db00f96225",
+    "url": "/counterscentral/index.html"
+  },
+  {
+    "revision": "0f6135dca7cb6f9012cf",
+    "url": "/counterscentral/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ab5ec3c91ea1633cad6f",
+    "url": "/counterscentral/static/css/main.f7290c29.chunk.css"
+  },
+  {
+    "revision": "0f6135dca7cb6f9012cf",
+    "url": "/counterscentral/static/js/2.bb822a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counterscentral/static/js/2.bb822a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab5ec3c91ea1633cad6f",
+    "url": "/counterscentral/static/js/main.fa5a4878.chunk.js"
+  },
+  {
+    "revision": "0e6a81bc4035c6cfc486",
+    "url": "/counterscentral/static/js/runtime-main.9843a90b.js"
+  }
+]);
